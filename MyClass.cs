58a3381612a4f5903cb59ee9688c5baf8697c728
Hi-tech <Hi-tech@DESKTOP-K8PC2B7>
2024-01-26 16:17:44 +0200

    Добавьте файлы проекта.

---
+++ MyClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class MyClass
+    {
+        
+        
+            private double value1;
+            private double value2;
+            public static int count;
+
+            public MyClass(double val1, double val2)
+            {
+                value1 = val1;
+                value2 = val2;
+                count++;
+            }
+
+            public MyClass()
+                : this(2.0, 2.0)
+            {
+            }
+
+            static MyClass()
+            {
+                count = 0;
+            }
+
+            public double Value1
+            {
+                get { return value1; }
+                set { value1 = value; }
+            }
+
+            public double Value2
+            {
+                get { return value2; }
+                set { value2 = value; }
+            }
+
+            public void DisplayValues()
+            {
+                Console.WriteLine($"Value1: {value1}, Value2: {value2}");
+            }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                
+                {
+                    MyClass obj1 = new MyClass(3.5, 4.2);
+                    MyClass obj2 = new MyClass();
+
+                    obj1.DisplayValues();
+                    obj2.DisplayValues();
+
+                    Console.WriteLine($"Total instances of MyClass: {MyClass.count}");
+                }
+            }
+        }
+    }
+}
